0b9b7c5d5ff89595db2894c19126f52ab0bca36e
weihao.li <bighead@leewaiho.com>
2026-07-10 08:46:30 +0800

    feat(keycloak): export AuthInk context adapter API

---
+++ src/keycloak/index.ts
@@ -0,0 +1,10 @@
+export { AuthInkKcPage } from "./AuthInkKcPage";
+export type {
+  AuthInkKcContext,
+  InfoKcContext,
+  KcMessage,
+  LoginKcContext,
+  RecoveryCodeKcContext,
+  ResetPasswordKcContext,
+  TotpKcContext,
+} from "./types";
